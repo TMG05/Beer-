f2f8c43a1425647e812c6408ec9dfc436337f93f
Tylian MAGRIT <tylian.magrit@epitech.eu>
2026-05-23 09:44:56 +0200

    feat: add DOM traversal for braille translation

---
+++ src/script.js
@@ -0,0 +1,17 @@
+const brailleDict = {
+    'a': '⠁', 'b': '⠃', 'c': '⠉', 'd': '⠙', 'e': '⠑',
+    'f': '⠋', 'g': '⠛', 'h': '⠓', 'i': '⠊', 'j': '⠚',
+    'k': '⠅', 'l': '⠇', 'm': '⠍', 'n': '⠝', 'o': '⠕',
+    'p': '⠏', 'q': '⠟', 'r': '⠗', 's': '⠎', 't': '⠞',
+    'u': '⠥', 'v': '⠧', 'w': '⠺', 'x': '⠭', 'y': '⠽', 'z': '⠵',
+    ' ': ' ', '1': '⠼⠁', '2': '⠼⠃', '3': '⠼⠉', '4': '⠼⠙', '5': '⠼⠑',
+    '6': '⠼⠋', '7': '⠼⠛', '8': '⠼⠓', '9': '⠼⠊', '0': '⠼⠚',
+    ',': '⠂', ';': '⠆', ':': '⠒', '.': '⠲', '!': '⠖', '?': '⠦'
+};
+
+function textToBraille(text) {
+    return text.toLowerCase().split('').map(char => {
+        const normalizedChar = char.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+        return brailleDict[normalizedChar] || char; 
+    }).join('');
+}
